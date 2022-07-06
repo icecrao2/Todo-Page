949b9225d8e863c746ede0da534c58da9779e960
문재웅 <63780449+icecrao2@users.noreply.github.com>
2022-07-06 20:20:10 +0900

    Update greetings.js

--- js/greetings.js
+++ js/greetings.js
@@ -1,6 +1,8 @@
 const loginForm=document.querySelector("#login-form");
 const loginInput=document.querySelector("#login-form input");
 const greeting=document.querySelector("#greeting");
+const toDo_Form = document.querySelector("#todo-form");
+
 
 const HIDDEN_CLASSNAME="hidden";
 const USERNAME_KEY="userName";
@@ -10,18 +12,21 @@ function onLoginSubmit(event){
   loginForm.classList.add(HIDDEN_CLASSNAME);
   const userName = loginInput.value;
   localStorage.setItem(USERNAME_KEY, userName);
+  toDo_Form.classList.remove(HIDDEN_CLASSNAME);
   paintGreetings(userName)
 }
 
 function paintGreetings(userName){
   greeting.innerText = `Hello ${userName}!`;
   greeting.classList.remove(HIDDEN_CLASSNAME);
+  toDo_Form.classList.remove(HIDDEN_CLASSNAME);
 }
 
 const savedUsername = localStorage.getItem(USERNAME_KEY);
 
 if(savedUsername === null){
   loginForm.classList.remove(HIDDEN_CLASSNAME);
+  toDo_Form.classList.add(HIDDEN_CLASSNAME);
   loginForm.addEventListener("submit", onLoginSubmit);
 }else{
   paintGreetings(savedUsername);
