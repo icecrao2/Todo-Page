607ff0b382421be2acafb36d5c2e245559894dba
문재웅 <63780449+icecrao2@users.noreply.github.com>
2022-07-09 19:48:04 +0900

    Add files via upload

---
+++ js/sizing.js
@@ -0,0 +1,9 @@
+let width = `${window.innerWidth}px`;
+
+window.addEventListener("resize",() => {
+  width = `${window.innerWidth}px`;
+  document.body.style.width=width;
+});
+
+width = `${window.innerWidth}px`;
+document.body.style.width=width;
